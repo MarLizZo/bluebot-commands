e63a4f3f4c596d4ad294b8300e3d5d4bce7a9212
Marco Cristofori <marlizzo78@gmail.com>
2026-04-26 09:59:40 +0200

    aliases release pkg

--- docs/assets/moderators-cmds-z17v-pKa.js
+++ docs/assets/moderators-cmds-z17v-pKa.js
@@ -1,9 +1,12 @@
-async function e(){try{let e=await fetch(`./moderators-cmds.json`);if(!e.ok)throw Error(`Error JSON moderators-cmds`);let t=await e.json(),n=document.getElementById(`moderators-cmds-container`);if(!n)return;n.innerHTML=t.map((e,t)=>{let n=t%2==0?``:`bg-even`,r=e.synthax?`<p><span class="font-semibold mt-3 synth">Synthax: </span>${e.synthax}</p>`:``;return`
+async function e(){try{let e=await fetch(`./moderators-cmds.json`);if(!e.ok)throw Error(`Error JSON moderators-cmds`);let t=await e.json(),n=document.getElementById(`moderators-cmds-container`);if(!n)return;n.innerHTML=t.map((e,t)=>{let n=t%2==0?``:`bg-even`,r=e.synthax?`<p><span class="font-semibold mt-3 synth">Synthax: </span>${e.synthax}</p>`:``,i=``;return e.aliases&&e.aliases.forEach((t,n)=>{i+=`<span>${t}</span>`,n!=e.aliases.length-1&&(i+=` - `)}),`
         <div class="flex w-full moderators-cmds ${n}">
           <div
                  class="line flex flex-col md:flex-row justify-center md:justify-start md:items-center w-3/12 md:w-2/12">
                  <i class="bi bi-copy fs-5 md:me-3 ps-1 md:ps-0" onclick="copyToClipboard('${e.command.toString()}', this)"></i>
-                 <p>${e.command}</p>
+                 <div class="flex flex-col pe-2 synth-container">
+                   <p>${e.command}</p>
+                   <p style="font-size: small">${i}</p>
+                 </div>
              </div>
             <div class="line flex flex-col justify-center w-3/12">
                  <p>${e.permissions}</p>
